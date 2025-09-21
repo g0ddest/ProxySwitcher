9751ba64849a181c2ececc53b3155d4bcad6b0dc
Vitaliy Velikodniy <vitaliy@velikodniy.name>
2025-09-21 10:31:50 +0300

    Initial Commit

---
+++ ProxySwitcher/ProxySwitcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProxySwitcherApp.swift
+//  ProxySwitcher
+//
+//  Created by Vitaliy Velikodniy on 21.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ProxySwitcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
